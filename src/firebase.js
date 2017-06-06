d557f3772d1d008d077534ae558f8a942baf1023
kndllbrwn <kndllbrwn1914@gmail.com>
2017-06-05 23:22:37 -0400

    first commit: scaffolded app and set up firebase project

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+var config = {
+    apiKey: "AIzaSyCkvdmR1ZejhOwIRgz6pnvZOy1txs0-qTY",
+    authDomain: "first-flight-with-friend-5cad4.firebaseapp.com",
+    databaseURL: "https://first-flight-with-friend-5cad4.firebaseio.com",
+    projectId: "first-flight-with-friend-5cad4",
+    storageBucket: "first-flight-with-friend-5cad4.appspot.com",
+    messagingSenderId: "943121771622"
+  };
+  
+  firebase.initializeApp(config);
